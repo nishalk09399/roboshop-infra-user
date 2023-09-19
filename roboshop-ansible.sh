2899fb108fb88162962f3528fb6de82653a6a3b5
nishal <nishalk9399@gmail.com>
2023-09-20 00:37:51 +0530

    pushing roboshop-infra-user files

---
+++ roboshop-ansible.sh
@@ -0,0 +1,18 @@
+#in this script we are automatic the roboshop whole project my git url only iam refering to 
+
+#!/bin/bash
+
+yum install ansible -y 
+cd /tmp
+git clone https://github.com/nishalk09399/roboshop-ansible-roles.git
+cd roboshop-ansible-roles
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=mongodb main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=mysql main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=rabbitmq main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=redis main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=user main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=catalogue main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=cart main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=shipping main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=payment main.yaml
+ansible-playbook -i inventory -e ansible_user=centos -e ansible_password=DevOps321 -e component=web main.yaml
